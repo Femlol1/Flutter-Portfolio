69867a817988a628dc0f7c6df7bb0d7bdb514006
Femi <osibemekunfemi@gmail.com>
2024-08-27 01:17:59 +0100

    flutter template

---
+++ lib/view/main/components/drawer/personal_info.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../../res/constants.dart';
+import 'header_info.dart';
+
+class PersonalInfo extends StatelessWidget {
+  const PersonalInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: defaultPadding / 2,
+        ),
+        AreaInfoText(title: 'Contact', text: '07377788552'),
+        AreaInfoText(title: 'Email', text: 'osibemekunfemi@gmail.com'),
+        AreaInfoText(title: 'LinkedIn', text: '@'),
+        AreaInfoText(title: 'Github', text: '@Femlol1'),
+        SizedBox(
+          height: defaultPadding,
+        ),
+        Text(
+          'Skills',
+          style: TextStyle(color: Colors.white),
+        ),
+        SizedBox(
+          height: defaultPadding,
+        ),
+      ],
+    );
+  }
+}
